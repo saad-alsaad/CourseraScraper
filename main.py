1de93fab87d34d7169676a250fa31b49bd0b1b0f
Saad Alsaad <50024334+saad-alsaad@users.noreply.github.com>
2022-05-16 21:41:58 +0300

    Push project

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import pandas as pd
 import requests as requests
 from bs4 import BeautifulSoup as bs
 import argparse
+from requests import Session
+
 
 COURSERA_URL = 'https://www.coursera.org/'
-COURSERA_QUERY_URL = f'{COURSERA_URL}search?query='
-
-session = requests.session()
 
 
 def parse_arguments():
+    """
+    This function return parsed arguments from the command line
+    :return: parsed arguments
+    """
     parser = argparse.ArgumentParser(description='Scraper Options.', prog='SCRIPT')
     parser.add_argument('--keyword', type=str, nargs=1, metavar='<Course Name>', help='Course keyword to look for', required=True)
     args = parser.parse_args()
@@ -20,109 +23,120 @@ def parse_arguments():
     return args
 
 
-def get_source(url: str) -> requests.Response:
-    """
-    Return the source code for the provided URL.
+class CourseraCourses:
+    _base_url: str = None
+    _session: Session = None
+    query_url = ''
+    keyword = ''
+    csv_file_name: str = 'results'
+    search_results = []
 
-    :param: url: URL of the page to scrape.
-    :return: response object: HTTP response object from requests_html.
-    """
+    def __init__(self, base_url):
+        self._base_url = base_url
+        self._session = requests.session()
+        self.query_url = f'{self._base_url}search?query='
 
-    try:
-        response = session.get(url)
-        return response
+    @staticmethod
+    def get_response(url: str, session: Session) -> requests.Response:
+        """
+        This method return the source code for the provided URL.
+        :param: url: URL of the page to scrape.
+        :return: response object: HTTP response object from requests_html.
+        """
 
-    except requests.exceptions.RequestException as e:
-        print(e)
+        try:
+            response = session.get(url)
+            return response
 
+        except requests.exceptions.RequestException as e:
+            print(f'Error happened when creating a request for {url}\nError Msg: {e}')
 
-def get_text_content(url: str) -> str:
-    response = get_source(url)
-    soup = bs(response.text.encode('utf-8'), 'html.parser')
-    soup = soup.find_all("script")
+    def _get_text_content(self, url: str) -> str:
+        """
+        This method return a JSON text for the Coursera page data
+        :param url: url for a specific page
+        :return: json text for page data
+        """
 
-    return soup[11].get_text()
+        response = self.get_response(url, self._session)
+        soup = bs(response.text.encode('utf-8'), 'html.parser')
+        soup = soup.find_all("script")
 
+        return soup[11].get_text()
 
-def get_courses_urls(url: str, page: int) -> list:
-    """
-    This function allow us to search courses in COURSERA_URL using a keyword and return list of courses urls
+    def _get_courses_urls(self, url: str, page: int) -> list:
+        """
+        This method allow us to search courses in self._base_url using a keyword and return list of courses urls
+        :param page: page number that we need to fetch results from
+        :param url: base URL and query
+        :return: list of courses url
+        """
 
-    :param page: page number that we need to fetch results from
-    :param url: base URL and query
-    :return: list of courses url
-    """
+        text = self._get_text_content(f'{url}&page={page}&index=prod_all_products_term_optimization&entityTypeDescription=Courses')
 
-    courses_url = []
+        data = text.split("window.App=")[1]
+        json_data = json.loads(data.split("window.appName=")[0][:-6])
+        results_state = json_data['context']['dispatcher']['stores']['AlgoliaResultsStateStore']['resultsState']
+        courses = results_state[2]['content']['_rawResults'][0]['hits']
 
-    text = get_text_content(f'{url}&page={page}&index=prod_all_products_term_optimization&entityTypeDescription=Courses')
+        courses_url = [self._base_url + course['objectUrl'] for course in courses]
 
-    data = text.split("window.App=")[1]
-    data = data.split("window.appName=")[0][:-6]
-    json_data = json.loads(data)
-    results_state = json_data['context']['dispatcher']['stores']['AlgoliaResultsStateStore']['resultsState']
-    courses = results_state[2]['content']['_rawResults'][0]['hits']
+        return courses_url
 
-    for course in courses:
-        courses_url.append(COURSERA_URL + course['objectUrl'])
+    def get_course_details(self, url: str) -> dict:
+        """
+        This method get the data for a specific course from self._base_url
+        :param url: a url for specific course
+        :return: a dictionary of course details, dictionary has (Name, Url, Rate, Tag, Description)
+        """
 
-    return courses_url
+        result = defaultdict()
 
+        text = self._get_text_content(url)
 
-def get_course_details(url: str) -> dict:
-    """
-    This function get the data for a specific course from  COURSERA_URL
+        json_data = json.loads(text)['@graph']
 
-    :param url: a url for specific course
-    :return: a dictionary of course details, dictionary has (Name, Url, Rate, Tag, Description)
-    """
+        course_content = json_data[1]
 
-    result = defaultdict()
+        result['Name'] = course_content['name']
+        result['Url'] = url
 
-    text = get_text_content(url)
+        result['Rating'] = None
+        if 'aggregateRating' in course_content:
+            rate_value = course_content['aggregateRating'].get('ratingValue')
+            if rate_value:
+                result['Rating'] = float(rate_value)
 
-    json_data = json.loads(text)['@graph']
+        result['Tag'] = [tag['item']['name'] for tag in json_data[0]['itemListElement'][1:]]
 
-    course_content = json_data[1]
+        result['Description'] = course_content['description']
 
-    result['Name'] = course_content['name']
-    result['Url'] = url
+        return result
 
-    result['Rating'] = None
-    if 'aggregateRating' in course_content:
-        rate_value = course_content['aggregateRating'].get('ratingValue')
-        if rate_value:
-            result['Rating'] = float(rate_value)
+    def get_coursera_courses(self, keyword: str):
+        """
+        This method implemented to get all courses details in the first page based on the passed keyword
+        :param keyword: a string to search for specific subject
+        :return: a list of courses details
+        """
 
-    result['Tag'] = []
-    for tag in json_data[0]['itemListElement'][1:]:
-        result['Tag'].append(tag['item']['name'])
+        self.keyword = keyword
+        urls = self._get_courses_urls(f'{self.query_url}{keyword}', 1)
+        self.search_results = [self.get_course_details(url) for url in urls]
 
-    result['Description'] = course_content['description']
+        return self.search_results
 
-    return result
+    def export_to_csv(self):
+        pd.DataFrame(self.search_results).to_csv(f'{self.keyword}_{self.csv_file_name}.csv', index=False, encoding='utf_8_sig')
 
-
-def get_coursera_courses(keyword: str) -> list:
-    """
-    This function implemented to get all courses details in the first page based on the passed keyword
-
-    :param keyword: a string to search for specific subject
-    :return: a list of courses details
-    """
-
-    courses_data = []
-    urls = get_courses_urls(f'{COURSERA_QUERY_URL}{keyword}', 1)
-
-    for url in urls:
-        courses_data.append(get_course_details(url))
-
-    return courses_data
+    def close_session(self):
+        if self._session:
+            self._session.close()
 
 
 if __name__ == '__main__':
     parse = parse_arguments()
-    courses_data = get_coursera_courses(parse.keyword)
-    pd.DataFrame(courses_data).to_csv('results.csv', index=False, encoding='utf_8_sig')
-    if session:
-        session.close()
+    coursera = CourseraCourses(COURSERA_URL)
+    results = coursera.get_coursera_courses(parse.keyword)
+    coursera.export_to_csv()
+    coursera.close_session()
--- tester.py
+++ tester.py
@@ -1,25 +1,27 @@
 import unittest
-import requests
-from main import get_coursera_courses, get_course_details
-
-
-session = requests.session()
+from main import CourseraCourses, COURSERA_URL
 
 
 class CheckCoursera(unittest.TestCase):
+    def setUp(self):
+        self.coursera = CourseraCourses(COURSERA_URL)
+
     def test_get_coursera_courses(self):
-        courses = get_coursera_courses('python')
+        courses = self.coursera.get_coursera_courses('python')
         self.assertEqual(len(courses), 10)
         self.assertEqual(courses[0]['Name'], 'Python for Everybody')
-        session.close()
+
+    def test_get_courses_urls(self):
+        urls = self.coursera._get_courses_urls(f'{self.coursera.query_url}{self.coursera.keyword}', 1)
+        self.assertEqual(len(urls), 10)
 
     def test_get_course_details(self):
-        result = get_course_details('https://www.coursera.org/learn/python-project-for-data-engineering')
+        result = self.coursera.get_course_details('https://www.coursera.org/learn/python-project-for-data-engineering')
         self.assertEqual(result['Name'], 'Python Project for Data Engineering')
         self.assertEqual(result['Url'], 'https://www.coursera.org/learn/python-project-for-data-engineering')
         self.assertEqual(len(result['Tag']), 2)
         self.assertIsNotNone(result['Rating'])
-        session.close()
+        self.coursera.close_session()
 
 
 if __name__ == '__main__':
